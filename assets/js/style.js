0a5a1601801266f4055dc6b7aac927d1ccbc4092
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2020-08-31 12:05:00 +0530

    Add files via upload

--- assets/js/style.js
+++ assets/js/style.js
@@ -204,54 +204,60 @@ fluteSampler.connect(fluteReverb);
       flutePart.remove(time,note);
     }
   });
-//main sequencer.
-  let sequencer = new Nexus.Sequencer('#sequencer',{
-    columns:32,
-    rows:12,
-    size:[1300,300]
+  //main sequencer.
+  let sequencer = new Nexus.Sequencer('#sequencer', {
+    columns: 32,
+    rows: 12,
+    size: [1300, 300]
   })
-  new Tone.Loop((time)=>{
-    Tone.Draw.schedule(()=>sequencer.next(),time)
-  },'16n').start();
-  
-  let sequencerRows=['C#3','E3','B3','G#3','C#2','E2','B2','G#2','C#1','E1','B1','G#1'];
+  new Tone.Loop((time) => {
+    Tone.Draw.schedule(() => sequencer.next(), time);
+  }, '16n').start();
+  let sequencerRows = ['B3', 'G#3', 'E3', 'C#3', 'B2', 'G#2', 'E2', 'C#2', 'B1', 'G#1', 'E1', 'C#1'];
   sequencer.on('change',({column,row,state})=>{
-    let time={'16n':column};
-    let note=sequencerRows[row];
-    if(state){
-      leadPart.add(time,note);
-    }
-    else{
-      leadPart.remove(time,note);
-    }
-  });
- /*//magenta stuff
-let melodyRnn=new music_rnn.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv');
-let melodyRnnLoaded=melodyRnn.initialize()
-document.getElementById('generate-melody').onclick=async()=>{
-await melodyRnnLoaded;
-  let seed={
-    notes:[
-      {pitch:Tone.Frequency('C#3').toMidi(),quantizedStartStep:0,quantizedEndStep:4}
+      let time={'16n':column};
+      let note=sequencerRows[row];
+      if(state){
+        leadPart.add(time,note);
+      }
+      else{
+        leadPart.remove(time,note);
+      }
+    });
+
+  
+ //magenta stuff 
+ let melodyRnn = new music_rnn.MusicRNN( 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv');
+let melodyRnnLoaded = melodyRnn.initialize()
+
+document.getElementById('generate-melody').onclick = async () => {
+  await melodyRnnLoaded;
+ 
+  let seed = {
+    notes: [
+      {pitch: Tone.Frequency('C#3').toMidi(), quantizedStartStep: 0, quantizedEndStep: 4}
     ],
-    totalQuantizedSteps:4,
-    quantizationInfo:{stepsPerQuarter:4}
-    
+    totalQuantizedSteps: 4,
+    quantizationInfo: { stepsPerQuarter: 4}
   };
-  let steps=28;
-  let temperature=1.2;
-  let chordProgression=['C#m7'];
-  let result=await melodyRnn.continueSequence(seed,steps,temperature,chordProgression);
-  let combined = core.sequences.concatenate([seed,result]);
+  let steps = 28;
+  let temperature = 1.2;
+  let chordProgression = ['C#m7'];
+  
+  let result = await melodyRnn.continueSequence(seed, steps, temperature, chordProgression);
+  
+  let combined = core.sequences.concatenate([seed, result]);
+  
   sequencer.matrix.populate.all([0]);
-  for(let note of combined.notes){
-    let column=note.quantizedStartStep;
-    let noteName=Tone.Frequency(note.pitch,'midi').toNote();
-    let row=sequencerRows.indexOf(noteName);
+  for (let note of combined.notes) {
+    let column = note.quantizedStartStep;
+    let noteName = Tone.Frequency(note.pitch, 'midi').toNote();
+    let row = ssequencerRows.indexOf(noteName);
+    if (row >= 0) {
+     sequencer.matrix.set.cell(column, row, 1);
+    }
+  }
+  console.log(combined);}
   
-  if(row>=0){
-    sequencer.matrix.set.cell(column,row,1);
-  }}
-  console.log(combined);
-  
-}*/
+
+    
